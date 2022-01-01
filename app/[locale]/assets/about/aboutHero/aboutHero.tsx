d8e971ca12ec2a0ae10e032ae9cd097b70351f3c
AbouCloud <Abouzari6839@gmail.com>
2022-01-01 01:17:16 +0000

    about  page updated

--- app/[locale]/assets/about/aboutHero/aboutHero.tsx
+++ app/[locale]/assets/about/aboutHero/aboutHero.tsx
@@ -1,20 +1,20 @@
 import { getTranslations } from "next-intl/server"
 import style from "./abouHero.module.css"
 
-export default async function AbouHero() {
-    const t = await getTranslations('abouthero')
-    const imageUrl  = t('heroImageUrl') || null
+export default async function AbouHero({message}: {message:any}) {
+    const t = await message.abouthero
+    const imageUrl  = t.heroImageUrl || null
     return (
         <div className={style.container}>
             <div className={style.cardImage}>
                 <div className={style.darkbackground}></div>
                 {
                     imageUrl && imageUrl.length>0  ?
-                    <img src={imageUrl} alt={t('heroImageAlt')} className={style.image} loading="lazy"/>
+                    <img src={imageUrl} alt={message.heroImageAlt} className={style.image} loading="lazy"/>
                     : 
                     <div className={style.image}></div> 
                 }
-                <h1 className={style.heroText}>{t('heroText')}</h1>
+                <h1 className={style.heroText}>{t.heroText}</h1>
             </div>
         </div>
 
