c4930ae1b111336d5b2de61adceba760b8a3ae95
AbouCloud <Abouzari6839@gmail.com>
2026-02-27 11:11:08 +0000

    the configuration of next-intl

---
+++ i18n/request.ts
@@ -0,0 +1,17 @@
+import { getRequestConfig } from "next-intl/server";
+import { routing } from "./routing";
+import { hasLocale } from "next-intl";
+
+
+export default getRequestConfig(async ({requestLocale}) => {
+  const requested = await requestLocale;
+  const locale = hasLocale(routing.locales, requested) ? requested : routing.defaultLocale
+const messages = {
+  ...(await import(`../messages/${locale}/common.json`)).default,
+  // ...(await import(`../messages/${locale}/home.json`)).default
+};
+  return {
+    locale,
+    messages
+  };
+});
