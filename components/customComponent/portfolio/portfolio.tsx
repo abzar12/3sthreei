4def8581bd55fb91463d33d6b03bfd889208573e
AbouCloud <Abouzari6839@gmail.com>
2026-03-16 17:03:42 +0000

    new page added portfolio pages and design

---
+++ components/customComponent/portfolio/portfolio.tsx
@@ -0,0 +1,39 @@
+import style from "./portfolio.module.css"
+import { getTranslations } from "next-intl/server"
+import { FaEye } from "react-icons/fa";
+
+type PortfolioSchema = {
+    img: string,
+    alt: string,
+    bgText: string
+}
+export default async function Portfolio() {
+    const t = await getTranslations()
+    const items = t.raw('portfolio') as PortfolioSchema[]
+    return (
+        <>
+            <div className={style.container}>
+                <h1 className={style.title}>Our Work</h1>
+                <div className={style.cards}>
+                    {
+                        items.map((item, index) => (
+                            <div key={index} className={style.card}>
+                                <div className={style.background}>
+                                    <FaEye className={style.icon}/>
+                                </div>
+                                {
+                                    item.img ?
+                                        <img className={style.img} src={item.img} alt={item.alt} height={200} width={350} loading="lazy" />
+                                        :
+                                        <div className={style.skeleton}>
+                                        </div>
+                                }
+                            </div>
+
+                        ))
+                    }
+                </div>
+            </div>
+        </>
+    )
+}
