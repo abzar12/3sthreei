642df2e989d83833422dc68bdf026b260402663b
AbouCloud <Abouzari6839@gmail.com>
2026-04-04 21:12:51 +0000

    process page added

---
+++ components/customComponent/process/PhoneProgress.tsx
@@ -0,0 +1,60 @@
+"use client"
+import style from "./process.module.css"
+import { useTranslations } from "next-intl"
+import { animate, motion, MotionValue, useMotionValue, useMotionValueEvent, useScroll } from "motion/react"
+import { FaLightbulb, FaCode, FaRocket, FaHandshake, FaPencilRuler, FaClipboardList, FaVial } from "react-icons/fa";
+import { useRef } from "react"
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious, } from "@/components/ui/carousel"
+type CardProps = {
+    icon: string,
+    title: string,
+    content: string
+}
+function PhoneProgress() {
+    const t = useTranslations()
+    const cards = t.raw('process') as CardProps[]
+    // const carouselRef = useRef(null)
+    // const { scrollXProgress } = useScroll({ container: carouselRef })
+    // useMotionValueEvent(scrollXProgress, "change", (v) => {
+    //     console.log(v)
+    // })
+    return (
+        <Carousel className={style.carousel}>
+            <CarouselContent>
+                {
+                    cards.map((item, index) => (
+                        <CarouselItem key={index} className={`${style.Phonecard} ${style[`card${index}`]}`}>
+                            <motion.div
+                                className={style.cardContainer}
+                                initial={{ opacity: 0, scale: 0.8, y: 50 }}
+                                whileInView={{ opacity: 1, scale: 1, y: 0 }}
+                                transition={{
+                                    duration: 0.5,
+                                    delay: index * 0.1, // 👈 nice stagger effect
+                                }}
+                                viewport={{ once: false }}
+                            >
+                                <div className={style.icons}>
+                                    {index === 0 && <FaLightbulb className={style.icon} />}
+                                    {index === 1 && <FaClipboardList className={style.icon} />}
+                                    {index === 2 && <FaPencilRuler className={style.icon} />}
+                                    {index === 3 && <FaCode className={style.icon} />}
+                                    {index === 4 && <FaVial className={style.icon} />}
+                                    {index === 5 && <FaRocket className={style.icon} />}
+                                    {index === 6 && <FaHandshake className={style.icon} />}
+                                </div>
+                                <h1 className={style.title}> <span className="text-[1.1rem] text-(--nav-text-color) hover:animate-bounce">{index + 1}</span> {item.title}</h1>
+                                <div className={style.content}>
+                                    {item.content}
+                                </div>
+                            </motion.div>
+                        </CarouselItem>
+                    ))
+                }
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+        </Carousel>
+    )
+}
+export default PhoneProgress
