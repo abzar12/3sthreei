bbf079ef835df8b3b0bfb867218bdbae5aecf76f
AbouCloud <Abouzari6839@gmail.com>
2026-04-17 07:40:28 +0000

    home page completed need updating

--- app/[locale]/assets/videoMarketing/videoMarketing.tsx
+++ app/[locale]/assets/videoMarketing/videoMarketing.tsx
@@ -1,23 +1,28 @@
 import { getTranslations } from "next-intl/server"
 import style from "./videoMarketing.module.css"
+import CustomVideoPlayer from "@/components/ui/video/customVideoPlayer"
 
-// type Props = {
-//     contentText: string,
-//     videoURL: string
-// }
 export default async function VideoMarketing(){
     const t = await getTranslations('mediaMarketing')
     return (
-        <div className={style.container}>
-            <div className={style.row}>
-                <div className={style.cardText}>
-                    <p>{t('contentText')}</p>
-                </div>
-                <div className={style.mediaSection}>
-                    <video src='#' height={200} width={200} autoPlay={false}>
-                    </video>
+        <section className={style.container}>
+            <div className={style.wrapper}>
+                <div className={style.row}>
+                    <div className={style.content}>
+                        <h2 className={style.title}>How We Work</h2>
+                        <div className={style.divider}></div>
+                        <p className={style.description}>
+                            {t('contentText')}
+                        </p>
+                        <button className={style.cta}>{t('cta')}</button>
+                    </div>
+                    <div className={style.videoWrapper}>
+                        <div className={style.videoContainer}>
+                            <CustomVideoPlayer Url="/e-commerce.mov"/>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
