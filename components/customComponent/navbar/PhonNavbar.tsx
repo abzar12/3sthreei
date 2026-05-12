12e0b7887b4e67fb36fb67dea84e795788f205a9
AbouCloud <Abouzari6839@gmail.com>
2026-05-12 13:35:20 +0000

    navbar updating

--- components/customComponent/navbar/PhonNavbar.tsx
+++ components/customComponent/navbar/PhonNavbar.tsx
@@ -5,7 +5,7 @@ import { FaBars, FaCode, FaGamepad, FaMobileAlt, FaPalette } from "react-icons/f
 import { TiArrowSortedDown } from "react-icons/ti";
 import { useState } from "react"
 import { NavbarProps } from "./Navbar";
-export default function PhoneNavbar({home, keyService, servicesValue, project, about, faq, blog, signin}: NavbarProps) {
+export default function PhoneNavbar({ home, keyService, servicesValue, project, about, faq, blog, signin }: NavbarProps) {
     // const t = useTranslations();
     const [is_active, setIs_active] = useState(false)
     const [openServices, setOpenServices] = useState(false)
@@ -13,43 +13,46 @@ export default function PhoneNavbar({home, keyService, servicesValue, project, a
         setOpenServices(!openServices)
     }
     const expand = () => {
-        setIs_active((prev)=> !prev)
+        setIs_active((prev) => !prev)
     }
     return (
         <div className={style.PhoneNav}>
-            <button className={style.btnMenu} onClick={expand}> <FaBars/> </button>
+            <button className={style.btnMenu} onClick={expand}> <FaBars /> </button>
             {
-                <ul className={`${style.PhoneNavlink} ${is_active && style.active}`}>
-                    <li> <Link href='/'>{home}</Link></li>
-                    <div className={style.li} onClick={serviceFn}>
-                    {/* <Link href='/service' > */}
-                    {keyService} <TiArrowSortedDown className={`${style.icon} ${openServices ? style.open : ''}`} />
-                    {
-                        openServices && (
-                            <div className={`${style.ServiceDropdown} ${openServices ? style.open : ''}`}>
-                                {
-                                    servicesValue.map((service, index) => {
-                                        const icons = [<FaCode />, <FaMobileAlt />, <FaGamepad />, <FaPalette />];
-                                        return (
-                                            <li key={service} className={style.serviceItem}>
-                                                <Link href='/projects' className={style.serviceLink}>
-                                                    {icons[index]} {service}
-                                                </Link>
-                                            </li>
-                                        );
-                                    })
-                                }
-                            </div>
-                        )
-                    }
-                    {/* </Link> */}
+                <div className={`${style.PhonenavCard} ${is_active && style.active}`} onClick={expand}>
+                    <ul className={`${style.PhoneNavlink}`}>
+                        <li > <Link href='/'>{home}</Link></li>
+                        <div className={style.li} onClick={serviceFn}>
+                            {/* <Link href='/service' > */}
+                            {keyService} <TiArrowSortedDown className={`${style.icon} ${openServices ? style.open : ''}`} />
+                            {
+                                // openServices && (
+                                    <div className={`${style.PhoneServiceDropdown} ${openServices ? style.open : ''}`}>
+                                        {
+                                            servicesValue.map((service, index) => {
+                                                const icons = [<FaCode />, <FaMobileAlt />, <FaGamepad />, <FaPalette />];
+                                                return (
+                                                    <li key={service} className={style.PhoneserviceItem}>
+                                                        <Link href='/projects' className={style.PhoneServiceLink}>
+                                                            {icons[index]} {service}
+                                                        </Link>
+                                                    </li>
+                                                );
+                                            })
+                                        }
+                                    </div>
+                                // )
+                            }
+                            {/* </Link> */}
+                        </div>
+                        <li> <Link href='/projects'>{project}</Link></li>
+                        <li> <Link href='/about'>{about}</Link></li>
+                        <li> <Link href='/faq'>{faq}</Link></li>
+                        <li> <Link href='/blog'>{blog}</Link></li>
+                        <li> <Link href='/blog'>{signin}</Link></li>
+                    </ul>
                 </div>
-                    <li> <Link href='/projects'>{project}</Link></li>
-                    <li> <Link href='/about'>{about}</Link></li>
-                    <li> <Link href='/faq'>{faq}</Link></li>
-                    <li> <Link href='/blog'>{blog}</Link></li>
-                    <li> <Link href='/blog'>{signin}</Link></li>
-                </ul>
+
             }
         </div>
     )
