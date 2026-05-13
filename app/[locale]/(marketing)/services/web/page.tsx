753d3b9491b0a3baebfec7d346c081af906d292e
AbouCloud <Abouzari6839@gmail.com>
2026-05-13 23:52:20 +0000

    services page added web

---
+++ app/[locale]/(marketing)/services/web/page.tsx
@@ -0,0 +1,12 @@
+import ServicesHero from "@/components/customComponent/servicesHero/ServicesHero";
+
+
+export default function Web(){
+    return (
+        <>
+        <section>
+            <ServicesHero/>
+        </section>
+        </>
+    )
+}
