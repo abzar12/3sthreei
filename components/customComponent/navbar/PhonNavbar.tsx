a692e860b37b7a6cb71c3cfc76fd78168d00a8d0
AbouCloud <Abouzari6839@gmail.com>
2026-05-13 22:10:02 +0000

    nav bar edited

--- components/customComponent/navbar/PhonNavbar.tsx
+++ components/customComponent/navbar/PhonNavbar.tsx
@@ -3,10 +3,10 @@ import style from "../navbar/navbar.module.css"
 import Link from "next/link"
 import { FaBars, FaCode, FaGamepad, FaMobileAlt, FaPalette } from "react-icons/fa";
 import { TiArrowSortedDown } from "react-icons/ti";
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
 import { NavbarProps } from "./Navbar";
 export default function PhoneNavbar({ home, keyService, servicesValue, project, about, faq, blog, signin }: NavbarProps) {
-    // const t = useTranslations();
+    const ControleExpand = useRef<HTMLUListElement | null>(null)
     const [is_active, setIs_active] = useState(false)
     const [openServices, setOpenServices] = useState(false)
     const serviceFn = () => {
@@ -15,32 +15,46 @@ export default function PhoneNavbar({ home, keyService, servicesValue, project, 
     const expand = () => {
         setIs_active((prev) => !prev)
     }
+    useEffect(() => {
+        const HandleExpand = (e: MouseEvent) => {
+            const target = e.target as Node | null
+            if (
+                ControleExpand.current && target && !ControleExpand.current.contains(target)
+            ) {
+                setIs_active(false)
+            }
+        }
+        document.addEventListener("mousedown", HandleExpand);
+        return () => {
+            document.removeEventListener("mousedown", HandleExpand)
+        }
+    }, [])
     return (
-        <div className={style.PhoneNav}>
+        <div className={style.PhoneNav} >
             <button className={style.btnMenu} onClick={expand}> <FaBars /> </button>
             {
-                <div className={`${style.PhonenavCard} ${is_active && style.active}`} onClick={expand}>
-                    <ul className={`${style.PhoneNavlink}`}>
+                <div className={`${style.PhonenavCard} ${is_active && style.active}`}>
+                    <ul className={`${style.PhoneNavlink}`} ref={ControleExpand}>
                         <li > <Link href='/'>{home}</Link></li>
                         <div className={style.li} onClick={serviceFn}>
                             {/* <Link href='/service' > */}
                             {keyService} <TiArrowSortedDown className={`${style.icon} ${openServices ? style.open : ''}`} />
                             {
                                 // openServices && (
-                                    <div className={`${style.PhoneServiceDropdown} ${openServices ? style.open : ''}`}>
-                                        {
-                                            servicesValue.map((service, index) => {
-                                                const icons = [<FaCode />, <FaMobileAlt />, <FaGamepad />, <FaPalette />];
-                                                return (
-                                                    <li key={service} className={style.PhoneserviceItem}>
-                                                        <Link href='/projects' className={style.PhoneServiceLink}>
-                                                            {icons[index]} {service}
-                                                        </Link>
-                                                    </li>
-                                                );
-                                            })
-                                        }
-                                    </div>
+                                <div className={`${style.PhoneServiceDropdown} ${openServices ? style.open : ''}`}>
+                                    {
+                                        servicesValue.map((service, index) => {
+                                            const icons = [<FaCode />, <FaMobileAlt />, <FaGamepad />, <FaPalette />];
+                                            return (
+                                                <li key={service} className={style.PhoneserviceItem}>
+                                                    <Link href='/projects' className={style.PhoneServiceLink}>
+                                                        {icons[index]} {service}
+                                                    </Link>
+                                                </li>
+                                            );
+                                        })
+                                    }
+                                </div>
                                 // )
                             }
                             {/* </Link> */}
