e0c2f3aeb5dd556821e41916c01728b3d1c24eb2
AbouCloud <Abouzari6839@gmail.com>
2026-05-16 19:17:09 +0000

    services page updated

---
+++ app/[locale]/(marketing)/services/application/page.tsx
@@ -0,0 +1,12 @@
+import ServicesHero from "@/components/customComponent/servicesHero/ServicesHero";
+
+
+export default function Application(){
+    return (
+        <>
+        <section>
+            <ServicesHero title="Custom mobile and deskop application" Planguage="Flutter • React Native • ElectronJS • JAVA"/>
+        </section>
+        </>
+    )
+}
---
+++ app/[locale]/(marketing)/services/design/page.tsx
@@ -0,0 +1,12 @@
+import ServicesHero from "@/components/customComponent/servicesHero/ServicesHero";
+
+
+export default function Design(){
+    return (
+        <>
+        <section>
+            <ServicesHero title="Custom web and application design" Planguage="Figma • Adobe XD"/>
+        </section>
+        </>
+    )
+}
