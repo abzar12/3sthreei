9449a06a099e8faee7b8386d5bbd7ea2389cdd37
AbouCloud <Abouzari6839@gmail.com>
2026-05-17 21:46:57 +0000

    home  Page updated

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -92,7 +92,7 @@ export default async function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
+      <Navbar />
         <NextIntlClientProvider messages={messages}>
           {children}
         </NextIntlClientProvider>
